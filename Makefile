51751d7f7ebb4de8aacafbccd1d49932995e5be7
sam <171685109+samgk13@users.noreply.github.com>
2026-04-15 00:58:00 +0530

    adding raw mode

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
+.PHONY: run
 build:
+	rm -f output
 	gcc src/ted.c -o output
 
-run:
+run: build
 	./output
--- src/ted.c
+++ src/ted.c
@@ -1,6 +1,33 @@
+#include <termios.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main(){
-  printf("this is a text editor trial\n");
+struct termios o_ted;
+
+void disableRawMode(){
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &o_ted);
+}
+
+void enableRawMode(){
+
+  tcgetattr(STDIN_FILENO, &o_ted);
+  atexit(disableRawMode);
+
+  struct termios raw = o_ted;
+  raw.c_lflag &= ~(ECHO | ICANON);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+}
+
+
+int main(void){
+
+  enableRawMode();
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')printf("%s",&c);
   return 0;
+
 }
