7b60bfa5f6d6e1b983e8e0eb2f393ed66b94b2f1
sam <171685109+samgk13@users.noreply.github.com>
2026-04-15 01:37:00 +0530

    input displaying

--- src/ted.c
+++ src/ted.c
@@ -1,6 +1,8 @@
-#include <termios.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <termios.h>
 #include <unistd.h>
 
 struct termios o_ted;
@@ -15,19 +17,34 @@ void enableRawMode(){
   atexit(disableRawMode);
 
   struct termios raw = o_ted;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_lflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+  raw.c_lflag &= ~(OPOST);
+  raw.c_lflag &= ~(CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 
 }
 
-
 int main(void){
 
   enableRawMode();
+  
+  while (1){
+    
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+    
+    if (iscntrl(c)){
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') break;
+  }
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')printf("%s",&c);
   return 0;
 
 }
