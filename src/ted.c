dc675b92801d98de7ee8bd987181b0eaa0630fe0
sam <171685109+samgk13@users.noreply.github.com>
2026-04-15 04:55:04 +0530

    part 1 of the custom terminal editor

--- src/ted.c
+++ src/ted.c
@@ -1,33 +1,51 @@
 /* includes */
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-
+#include <sys/ioctl.h>
+#include <string.h>
 #include <termios.h>
 #include <unistd.h>
 
+/* defines */
+
+#define CTRL_KEY(k) ((k) & 0x1f) 
+
+#define TED_VERSION "0.0.1"
+
 /* data */
 
-struct termios o_ted;
+struct editorConfig {
+  int cx, cy;
+  int screenrows;
+  int screencols;
+  struct termios o_ted;
+};
+
+struct  editorConfig E;
 
 /* terminal */
 
 void die(const char *s){
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\xb[H", 3);
+
   perror(s);
   exit(1);
 }
 
 void disableRawMode(){
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &o_ted) == -1) die("tcsetattr");
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.o_ted) == -1) die("tcsetattr");
 }
 
 void enableRawMode(){
 
-  if (tcgetattr(STDIN_FILENO, &o_ted) == -1) die("tcsetattr");
+  if (tcgetattr(STDIN_FILENO, &E.o_ted) == -1) die("tcsetattr");
   atexit(disableRawMode);
 
-  struct termios raw = o_ted;
+  struct termios raw = E.o_ted;
   raw.c_lflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
   raw.c_lflag &= ~(OPOST);
   raw.c_lflag &= ~(CS8);
@@ -39,24 +57,155 @@ void enableRawMode(){
 
 }
 
+char editorReadKey(){
+  
+  int nread;
+  char c;
+
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1){
+    if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+int getCursorPositer(int *rows, int *cols){
+  
+  char buf[32];
+  unsigned int i = 0;
+
+  if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+  while (i < sizeof(buf) - 1){
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
+  }
+  buf[i] = '\0';
+  
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+  return 0;
+}
+
+int getWindowSize(int *rows, int *cols){
+  struct winsize ws;
+
+  if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+    return getCursorPositer(rows, cols);
+  } else {
+    *cols =ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
+/* append buffer */
+
+struct abf {
+  char *b;
+  int len;
+};
+
+#define ABF_INIT {NULL, 0}
+
+void abAppend(struct abf *ab, const char *s, int len){
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abf *ab){
+  free(ab->b);
+}
+
+/* input */
+
+void editorProcessKeypress(){
+
+  char c = editorReadKey();
+
+  switch (c) {
+    case CTRL_KEY('q'):
+      
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\xb[H", 3);
+    
+      exit(0);
+      break;
+  }
+}
+
+/* output */
+
+void editorDrawRows(struct abf *ab){
+
+  int y;
+  for (y=0; y < E.screenrows; y++){
+    if (y == E.screenrows / 3){
+      
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+        "Ted-iter -- version %s", TED_VERSION);
+      
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~",1);
+        padding--;
+      }
+
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    
+    } else {
+      abAppend(ab, "~", 1);
+    }
+    
+    abAppend(ab, "\x1b[K", 3);
+    if (y < E.screenrows - 1) {
+      abAppend(ab, "\r\n", 2);
+    }
+  }
+
+}
+
+void editorRefreshScreen(){
+  struct abf ab = ABF_INIT;
+
+  abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
+
+  editorDrawRows(&ab);
+
+  abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
+}
+
 /* initialize */
+
+void initEditor(){
+  E.cx = 0;
+  E.cy = 0;
+
+  if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
 
 int main(void){
 
   enableRawMode();
+  initEditor();
   
   while (1){
-    
-    char c = '\0';
-    
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-    
-    if (iscntrl(c)){
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q') break;
+    editorRefreshScreen();
+    editorProcessKeypress();  
   }
 
   return 0;
